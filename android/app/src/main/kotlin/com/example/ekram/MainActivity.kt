01d6cf65f58213e4855968e3bf51e811b4f2b247
Emam999 <mdemamshikder1@gmail.com>
2021-10-09 00:45:38 -0600

    First commit

---
+++ android/app/src/main/kotlin/com/example/ekram/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ekram
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+String btnText1="Button One";
+Color btnColor1=Colors.purple;
+Color btnTextColor1=Colors.white;
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+     home: MyHomePage(),
+    );
+  }
+}
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.cyan,
+        centerTitle: true,
+        title: Text("Ekram Khan",style: TextStyle(color: Colors.black),),
+      ),
+      body:
+      SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              RaisedButton(
+              color: btnColor1,
+                child: Text(btnText1,style:TextStyle(color:btnTextColor1),),
+                  onPressed:(){
+                    setState(() {
+                      btnColor1=Colors.green;
+                      btnText1="button Pressed";
+                      btnTextColor1=Colors.black;
+                    });
+
+
+              }),
+              Image.network("https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510__480.jpg")
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
